6d87e5ae5f206ba5647a0b67ebfb1ea2a199383f
Muhammad Hasham <hasham25525@gmail.com>
2023-09-02 03:26:41 +0500

    product info page

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -5,9 +5,31 @@ import { SanityProduct } from "@/config/inventory"
 import { ProductGallery } from "@/components/product-gallery"
 import { ProductInfo } from "@/components/product-info"
 
-interface Props {}
+interface Props {
+  params: {
+    slug: string
+  }
+}
 
-export default function Page() {
+export default async function Page({ params }: Props) {
+ const product= await client.fetch<SanityProduct>(
+    groq`*[_type== "product" && slug.current =="${params.slug}"][0]{
+      _id,
+      _createdAt,
+      "_id":_id,
+      name,
+      sku,
+      images,
+      currency,
+      price,
+      description,
+      sizes,
+      categories,
+      colors,
+      "slug": slug.current
+    }`
+  )  
+
   return (
     <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
       <div className="mx-auto max-w-2xl lg:max-w-none">
@@ -15,6 +37,8 @@ export default function Page() {
         <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
           {/* Product gallery */}
           {/* Product info */}
+          <ProductInfo product={product}/>
+          
         </div>
       </div>
     </main>
