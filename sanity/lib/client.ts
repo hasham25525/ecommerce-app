16204ccf9cfaa8f79de1ba8cb2aab5ba68bfe915
Muhammad Hasham <hasham25525@gmail.com>
2023-09-04 02:52:55 +0500

    token removed

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skQDrsbHba4pBfAVh8btA9kgdq7zcZhwfWzYHkxuD6hqMUEJUNQaJkqDTh4PT6SCwCUXkUXzxYlaQSND1j1KCnHNLZRNRL5UUMlJJUCD4xpHAS8Qvh60VWDhv9JlV4BdSFksQFBrk2mxGO5MpNem8nfy1hlZICVBrt6AK1sbRFRPJkFFIOrH",
-})
+  })
