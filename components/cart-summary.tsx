282fa1672374862a7b878817a1a2832ab6a9947d
Muhammad Hasham <hasham25525@gmail.com>
2023-09-04 02:31:11 +0500

    onCheckout functionality

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -1,21 +1,40 @@
 "use client"
 
+import { error } from "console"
 import { useState } from "react"
 import { Loader2 } from "lucide-react"
+import { set } from "sanity"
 import { formatCurrencyString, useShoppingCart } from "use-shopping-cart"
 
 import { Button } from "@/components/ui/button"
 
 export function CartSummary() {
-  const { formattedTotalPrice, totalPrice, cartDetails, cartCount } =
-    useShoppingCart()
+  const {
+    formattedTotalPrice,
+    totalPrice,
+    cartDetails,
+    cartCount,
+    redirectToCheckout,
+  } = useShoppingCart()
   const [isLoading, setLoading] = useState(false)
   const isDisable = isLoading || cartCount! === 0
 
   const shippingAmount = cartCount! > 0 ? 500 : 0
   const totalAmount = totalPrice! + shippingAmount
 
-  function onCheckout() {}
+  async function onCheckout() {
+    setLoading(true)
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      body: JSON.stringify(cartDetails),
+    })
+    const data = await response.json()
+    const result = await redirectToCheckout(data.id)
+    if (result?.error) {
+      console.error(result)
+    }
+    setLoading(false)
+  }
 
   return (
     <section
